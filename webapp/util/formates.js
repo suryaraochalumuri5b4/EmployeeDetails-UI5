6f7921da281b4d92489cfa95e856970b2999905e
SURYA RAO CHALUMURI <suryaraochalumuri5b4@gmail.com>
2026-07-18 13:49:01 +0000

    Added New colum EmployeeDob and formatted mm-dd-yyyy

--- webapp/util/formates.js
+++ webapp/util/formates.js
@@ -1,5 +1,7 @@
-sap.ui.define(['sap/ui/core/format/NumberFormat'],
-    function(NumberFormat){
+sap.ui.define(['sap/ui/core/format/NumberFormat',
+                'sap/ui/core/format/DateFormat'
+],
+    function(NumberFormat,DateFormat){
         
         return{
             formatEmpName:function(inp){
@@ -12,6 +14,21 @@ sap.ui.define(['sap/ui/core/format/NumberFormat'],
         formatCurrency:function(amount,currency){
             var oCurrencyFormat = NumberFormat.getCurrencyInstance();
             return oCurrencyFormat.format(amount,currency);
+
+        },
+        formatDate:function(date){
+          var oDate;
+        // If date is a string
+        if (typeof date === "string") {
+            oDate = new Date(date.replace(" ", "T"));
+        } else {
+            oDate = date;
+        }
+
+        var oDateFormat = DateFormat.getDateInstance({
+           pattern: "MM-dd-yyyy"
+        });
+        return oDateFormat.format(oDate);
 
         }
 
